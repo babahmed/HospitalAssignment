78d0e4ad106952be58d4639ed5afdf0b7d93e518
babahmed <ba21414@essex.ac.uk>
2022-02-14 17:47:08 +0100

    current task

---
+++ main.py
@@ -0,0 +1,9 @@
+from phone_record import classes
+
+
+def main():
+    phone_operations()
+
+
+if __name__ == "__main__":
+    main()
---
+++ objects.py
@@ -0,0 +1,6 @@
+
+def AppointmentSchedule():
+    p = Point()
+    p.x = rect.corner.x + rect.width/2
+    p.y = rect.corner.y + rect.height/2
+    return p
